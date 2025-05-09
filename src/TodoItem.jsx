48e5b692bf58c82e9b2ef718e09344812f12d9df
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-10 00:05:22 +0900

    style: 삭제 버튼 스타일 적용

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,7 +4,12 @@ const TodoItem = ({ todo, onDelete }) => {
   return (
     <li className="flex justify-between w-100 text-2xl text-center px-3 py-5 border-b-2">
       <span>{todo.text}</span>
-      <button onClick={onDelete}>삭제</button>
+      <button
+        onClick={onDelete}
+        className="text-lg text-black border-2 border-black bg-white px-1.5 py-1 rounded-lg cursor-pointer hover:bg-red-400 hover:text-white"
+      >
+        삭제
+      </button>
     </li>
   );
 };
