d1b59d29190d3f33c528386fd8ac8d31d13123b3
chominseo <singer100@yu.ac.kr>
2025-05-09 17:21:09 +0900

    FE 7차시 과제

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, onDelete }) => {
   return (
     <li className="w-100 text-2xl text-center px-3 py-5 border-b-2">
       <span>{todo.text}</span>
+        <button
+        onClick={onDelete}
+        className="ml-10 px-5 py-2 text-sm font-bold text-white bg-blue-700 rounded hover:bg-red-600"
+      >
+        삭제
+      </button>
     </li>
   );
 };
