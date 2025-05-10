217edec80a2185ee98576ae075c344e3d8738242
MinJeong <mineun0517@naver.com>
2025-05-11 03:39:58 +0900

    삭제 기능 구현

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, onDelete }) => {
   return (
     <li className="w-100 text-2xl text-center px-3 py-5 border-b-2">
       <span>{todo.text}</span>
+      <button
+        type="button"
+        onClick={() => onDelete(todo.id)}
+        className="ml-60 text-sm px-2 py-1 bg-white rounded-xl cursor-pointer border-2"
+      >
+        삭제
+      </button>
     </li>
   );
 };
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -30,6 +30,10 @@ const TodoList = () => {
     setError(false);
   };
 
+  const handleDelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="bg-gray-200 h-screen flex flex-col items-center">
       <h2 className="text-5xl font-bold my-10">To-Do List</h2>
@@ -56,7 +60,7 @@ const TodoList = () => {
       </form>
       <ul>
         {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem key={todo.id} todo={todo} onDelete={handleDelete} />
         ))}
       </ul>
     </div>
