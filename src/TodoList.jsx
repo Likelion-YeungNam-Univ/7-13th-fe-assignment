40531606917d0fa688954af40abea4f63251e621
gyeongsangseaman <sadx302@gmail.com>
2025-05-10 23:07:25 +0900

    모달창 구현

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -29,10 +29,15 @@ const TodoList = () => {
     setTodos((prev) => [...prev, { id: Date.now(), text: inputText }]);
     setInputText("");
     setError(false);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <div className="bg-gray-200 h-screen flex flex-col items-center">
+    <div className="bg-gray-200 h-screen flex flex-col items-center relative">
       <h2 className="text-5xl font-bold my-10">To-Do List</h2>
       <form onSubmit={handleAdd} className="flex gap-2 mb-5 relative">
         <input
@@ -60,6 +65,17 @@ const TodoList = () => {
           <TodoItem key={todo.id} todo={todo} />
         ))}
       </ul>
+
+      {isModalOpen && (
+        <div
+          className="fixed inset-0 bg-black/75 flex items-center justify-center"
+          onClick={closeModal}
+        >
+          <div className="bg-white p-5 rounded-lg min-w-[300px] text-center">
+            <p className="text-2xl font-bold">할 일이 추가되었습니다.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
