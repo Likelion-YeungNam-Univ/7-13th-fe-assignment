ae668f28a2f8da876502f853bcbefa41d08c1c34
gyeongsangseaman <sadx302@gmail.com>
2025-05-11 00:09:07 +0900

    삭제 버튼 구현 및 주석 정리

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, todoDelete }) => {
   return (
-    <li className="w-100 text-2xl text-center px-3 py-5 border-b-2">
+    <li className="flex justify-between w-100 text-2xl text-center px-3 py-5 border-b-2">
       <span>{todo.text}</span>
+      {/* 삭제 버튼 */}
+      <button
+        className="bg-white border-2 rounded-lg text-lg px-2 py-1"
+        onClick={todoDelete} // 클릭됐을 때, todoDelete (= handleDelete) 함수를 실행
+      >
+        삭제
+      </button>
     </li>
   );
 };
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,7 +5,7 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [inputText, setInputText] = useState("");
   const [error, setError] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // isModalOpen 상태(state)를 선언, 초기값은 false로 설정
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
@@ -29,11 +29,18 @@ const TodoList = () => {
     setTodos((prev) => [...prev, { id: Date.now(), text: inputText }]);
     setInputText("");
     setError(false);
-    setIsModalOpen(true);
+    setIsModalOpen(true); // 제출하면 isModalOpen을 true로 설정하여 모달창이 띄워지도록
   };
 
+  // 모달창을 끄는 closeModal 함수
   const closeModal = () => {
-    setIsModalOpen(false);
+    setIsModalOpen(false); // isModalOpen을 false로 설정하여 모달창이 꺼지도록
+  };
+
+  // todo를 삭제하는 handleDelete 함수
+  const handleDelete = (index) => {
+    const temp = todos.filter((_, i) => i !== index); // filter()를 통해 배열의 각 요소를 검사하여 index가 조건과 다른 요소만으로 새로운 배열 반환, 그걸 temp에 넣고,
+    setTodos(temp); // temp를 todos로 설정
   };
 
   return (
@@ -61,19 +68,26 @@ const TodoList = () => {
         </button>
       </form>
       <ul>
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            todoDelete={() => handleDelete(index)} // handleDelete 함수를 props로 전달, 화살표 함수로 감싸서 호출 지연
+          />
         ))}
       </ul>
 
+      {/* 모달창 띄우기, isModalOpen이 true일 때 실행됨 */}
       {isModalOpen && (
+        // 배경
         <div
           className="fixed inset-0 bg-black/75 flex items-center justify-center"
-          onClick={closeModal}
+          onClick={closeModal} // 클릭됐을 때, closeModal 함수를 실행하여 모달창이 꺼짐
         >
+          {/* 모달창 */}
           <div
             className="bg-white p-15 rounded-lg text-center"
-            onClick={(e) => e.stopPropagation()}
+            onClick={(e) => e.stopPropagation()} // 이게 포함된 태그는 이벤트가 발생하지 않음(?) -> 배경을 클릭하면 모달창이 꺼지는데, 모달창 내부를 클릭하면 꺼지지 않음
           >
             <p className="text-2xl font-bold">할 일이 추가되었습니다!</p>
           </div>
